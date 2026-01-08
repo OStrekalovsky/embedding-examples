867de8dbdcba60598a9985024d5cdd9b5651eeaa
ostrekalovsky <o.strekalovsky@yandex.ru>
2026-01-08 10:41:22 +0300

    fix  nil_receiver example

--- nil_receiver/embedded_cart_nil_receiver_test.go
+++ nil_receiver/embedded_cart_nil_receiver_test.go
@@ -8,25 +8,29 @@ import (
 
 func TestWrapped(t *testing.T) {
 	tests := []struct {
+		name string
 		cfg  CfgWithWrappedCart
 		want string
 	}{
 		{
+			name: "nil outer",
 			cfg:  CfgWithWrappedCart{c: nil},
 			want: "Empty cart",
 		},
 		{
+			name: "nil inner",
 			cfg:  CfgWithWrappedCart{c: &RenderedCart{Cart: nil}},
 			want: "Empty cart",
 		},
 
 		{
+			name: "regular",
 			cfg:  CfgWithWrappedCart{c: &RenderedCart{Cart: &Cart{[]int{1, 2, 3}}, tabID: 1}},
 			want: "Cart items:[1 2 3]",
 		},
 	}
 	for _, test := range tests {
-		t.Run(test.want, func(t *testing.T) {
+		t.Run(test.name, func(t *testing.T) {
 			got := renderWrappedCart(test.cfg)
 			assert.Equal(t, test.want, got)
 		})
